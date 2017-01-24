e9e49901d1d11f21e7cc81f647d00c9959208026
Jeff Bishop <jeffbshp@gmail.com>
2017-01-24 16:57:23 -0600

    post and view messages

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var cool = require('cool-ascii-faces');
 var pg = require('pg');
+var multiparty = require('multiparty');
+var util = require('util');
 var express = require('express');
 var app = express();
+
+const q_get_messages = 'SELECT * FROM Messages ORDER BY id DESC';
+const q_post_message = 'INSERT INTO Messages(username, message) values ($1, $2)';
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -9,33 +13,49 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-    var result = '';
-    var times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + ' ';
-    }
-  response.send(result);
-});
-
-app.get('/db', function(request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+  pg.connect(process.env.DATABASE_URL, function(error, client, done) {
+    client.query(q_get_messages, function(error, result) {
       done();
-      if (err) {
-        console.console.error(err);
-        response.send("Error " + err);
+      if (error) {
+        console.error(error);
+        response.send("Error " + error);
       } else {
-        response.render('pages/db', {results: result.rows});
+        response.render('pages/index', {results: result.rows});
       }
     });
+  });
+});
+
+app.post('/', function(request, response) {
+  var form = new multiparty.Form();
+  form.parse(request, function(error, fields, files) {
+    if (error) {
+      response.writeHead(400, {'content-type': 'text/plain'});
+      response.end('Invalid request: ' + error.message);
+      return;
+    }
+
+    pg.connect(process.env.DATABASE_URL, function(error, client, done) {
+      client.query(q_post_message, [fields.username[0], fields.message[0]], function(error, result) {
+        done();
+        if (error) {
+          console.error(error);
+          response.send("Error " + error);
+        } else {
+          client.query(q_get_messages, function(error, result) {
+            done();
+            if (error) {
+              console.error(error);
+              response.send("Error " + error);
+            } else {
+              response.render('pages/index', {results: result.rows});
+            }
+          });
+        }
+      });
+    });
+
+    console.log('Name: ', fields.username[0], ', Message: ', fields.message[0]);
   });
 });
 
