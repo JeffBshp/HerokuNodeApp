f2967e42fad3a37ca9d778fc00dde4eeaa970336
Jeff Bishop <jeffbshp@gmail.com>
2017-02-01 14:58:58 -0600

    limited number of messages to load at a time; added buttons to get latest messages and older messages

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const mustacheExpress = require('mustache-express');
 const util = require('util');
 const express = require('express');
 
-const q_get_messages = 'SELECT * FROM Messages ORDER BY id ASC';
+const q_get_last_id = 'SELECT id FROM Messages ORDER BY id DESC LIMIT 1';
+const q_get_messages = 'SELECT * FROM Messages WHERE id > $1 AND id < $2 ORDER BY id ASC';
 const q_post_message = 'INSERT INTO Messages(username, message) VALUES ($1, $2) RETURNING id, username, message';
 const username_max_length = 50;
 const message_max_length = 5000;
+const initial_message_count = 20;
+const max_messages_to_send = 40;
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -16,22 +19,63 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+var lastId = 0;
+getLastId();
+
+function getLastId() {
+    pg.connect(process.env.DATABASE_URL, function(error, client, done) {
+		client.query(q_get_last_id, function(error, result) {
+			done();
+			if (error) {
+				console.error(error);
+			} else {
+				lastId = Number(result.rows[0].id);
+                console.log('Last message ID: ' + lastId);
+			}
+		});
+	});
+}
+
+function getMessages(fromId, toId, response) {
+    pg.connect(process.env.DATABASE_URL, function(error, client, done) {
+		client.query(q_get_messages, [fromId, toId], function(error, result) {
+			done();
+			if (error) {
+				console.error(error);
+                response.send(error);
+			} else {
+                response.send(result.rows);
+			}
+		});
+	});
+}
+
 app.get('/', function(request, response) {
 	response.render('pages/index');
 });
 
 app.get('/messages', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(error, client, done) {
-		client.query(q_get_messages, function(error, result) {
-			done();
-			if (error) {
-				console.error(error);
-				response.send("Error " + error);
-			} else {
-				response.send(result.rows);
-			}
-		});
-	});
+    var fromId = lastId - initial_message_count;
+    var toId = lastId + 1;
+    getMessages(fromId, toId, response);
+});
+
+app.get('/messages/:fromId', function(request, response) {
+    var fromId = Number(request.params.fromId);
+    var toId = lastId + 1;
+    if (toId - fromId - 1 > max_messages_to_send) {
+        toId = fromId + max_messages_to_send + 1;
+    }
+    getMessages(fromId, toId, response);
+});
+
+app.get('/messages/:fromId/:toId', function(request, response) {
+    var fromId = Number(request.params.fromId);
+    var toId = Number(request.params.toId);
+    if (toId - fromId - 1 > max_messages_to_send) {
+        fromId = toId - max_messages_to_send - 1;
+    }
+    getMessages(fromId, toId, response);
 });
 
 app.post('/messages', function(request, response) {
@@ -56,9 +100,10 @@ app.post('/messages', function(request, response) {
 					done();
 					if (error) {
 						console.error(error);
-						response.send("Error " + error);
+						response.send(error);
 					} else {
-                        console.log('ID: ' + result.rows[0].id + ', Name: "' + username + '", Message: "' + message + '"');
+                        lastId = Number(result.rows[0].id);
+                        console.log('ID: ' + lastId + ', Name: "' + username + '", Message: "' + message + '"');
 						response.send(result.rows[0]);
 					}
 				});
@@ -86,7 +131,7 @@ app.post('/messages', function(request, response) {
 	// 				done();
 	// 				if (error) {
 	// 					console.error(error);
-	// 					response.send("Error " + error);
+	// 					response.send(error);
 	// 				} else {
 	// 					response.send(result);
 	// 				}
--- public/scripts/message.js
+++ public/scripts/message.js
@@ -6,6 +6,14 @@ $(function() {
     var $messageList = $('#message-list');
     var $usernameInput = $('#username');
     var $messageInput = $('#message');
+    var $submit = $('#submit');
+    var $refresh = $('#refresh');
+    var $top = $('#top');
+    var $loadMore = $('#load-more');
+    
+    var firstId = 0;
+    var lastId = 0;
+    var messagesPerRequest = 10;
     
     const message_template =
     '<li>' +
@@ -15,7 +23,7 @@ $(function() {
 					'<div class="col-md-6 col-sm-6 col-xs-6">' +
 						'<h3 class="panel-title">{{username}}</h3>' +
 					'</div>' +
-					'<div class="col-md-6 col-sm-6 col-xs-6 message-id">' +
+					'<div class="col-md-6 col-sm-6 col-xs-6 right-aligned-col">' +
 						'<h3 class="panel-title">{{id}}</h3>' +
 					'</div>' +
 				'</div>' +
@@ -26,22 +34,66 @@ $(function() {
 		'</div>' +
 	'</li>';
     
-    function addMessage(message) {
-        $messageList.prepend(Mustache.render(message_template, message));
+    function prependMessages(messages) {
+        $.each(messages, function(i, message) {
+            $messageList.prepend(Mustache.render(message_template, message));
+        });
     }
     
-    $.ajax({
-        type: 'GET',
-        url: '/messages',
-        success: function(messages) {
-            $.each(messages, function(i, message) {
-                addMessage(message);
-            });
-        },
-        failure: function() {}
-    });
+    function appendMessages(messages) {
+        for (var i = messages.length - 1; i >= 0; i--) {
+            $messageList.append(Mustache.render(message_template, messages[i]));
+        }
+    }
     
-	$('#submit').on('click', function() {
+    function getMessages() {
+        $.ajax({
+            type: 'GET',
+            url: '/messages/',
+            success: function(messages) {
+                if (messages.length > 0) {
+                    prependMessages(messages);
+                    firstId = Number(messages[0].id);
+                    lastId = Number(messages[messages.length-1].id);
+                } else {
+                    //
+                }
+            },
+            failure: function() {}
+        });
+    }
+    
+    function getNewMessages() {
+        $.ajax({
+            type: 'GET',
+            url: '/messages/' + lastId,
+            success: function(messages) {
+                if (messages.length > 0) {
+                    prependMessages(messages);
+                    lastId = Number(messages[messages.length-1].id);
+                }
+            },
+            failure: function() {}
+        });
+    }
+    
+    function getOldMessages() {
+        $.ajax({
+            type: 'GET',
+            url: '/messages/' + (firstId - messagesPerRequest - 1) + '/' + firstId,
+            success: function(messages) {
+                if (messages.length > 0) {
+                    appendMessages(messages);
+                    firstId = Number(messages[0].id);
+                }
+            },
+            failure: function() {}
+        });
+    }
+    
+    getMessages();
+    
+	$submit.on('click', function() {
 		var username = $usernameInput.val();
 		var message = $messageInput.val();
 		if (username.length > username_max_length || message.length == 0 || message.length > message_max_length) {
@@ -56,11 +108,27 @@ $(function() {
 				url: '/messages',
 				data: data,
 				success: function(newMessage) {
-                    addMessage(newMessage);
+                    //addMessage(newMessage);
+                    getNewMessages();
                     $messageInput.val('').focus();
                 },
 				error: function() {}
 			});
 		}
 	});
+    
+    $refresh.on('click', function() {
+        getNewMessages();
+        return false;
+    });
+    
+    $top.on('click', function() {
+        window.scrollTo(0, 0);
+        return false;
+    });
+    
+    $loadMore.on('click', function() {
+        getOldMessages();
+        return false;
+    });
 });
